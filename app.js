da0006c1a5afff931d86465566b95d84763b2950
ajstrand <ajstrand8@gmail.com>
2017-09-24 17:18:17 -0400

    added helmet for security

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var http = require('http');
 var bodyParser = require('body-parser');
+var helmet = require('helmet')
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -13,6 +14,7 @@ var app = express();
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
